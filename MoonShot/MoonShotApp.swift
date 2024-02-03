858ec2dd44e5ab692ed902036756fb23586eee5a
Lexter T <lexter.tapawan@gmail.com>
2024-02-03 20:51:42 +0000

    Initial Commit

---
+++ MoonShot/MoonShotApp.swift
@@ -0,0 +1,17 @@
+//
+//  MoonShotApp.swift
+//  MoonShot
+//
+//  Created by Lexter Tapawan on 03/02/2024.
+//
+
+import SwiftUI
+
+@main
+struct MoonShotApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
