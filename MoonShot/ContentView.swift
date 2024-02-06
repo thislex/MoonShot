364a2cf7a875d76480d38fd7c4b04b87cc3d58c5
Lexter T <lexter.tapawan@gmail.com>
2024-02-06 11:50:06 +0000

    Clean project without sample codes.

--- MoonShot/ContentView.swift
+++ MoonShot/ContentView.swift
@@ -7,106 +7,10 @@
 
 import SwiftUI
 
-//struct CustomText: View {
-//    let text: String
-//
-//    var body: some View {
-//        Text(text)
-//    }
-//
-//    init(_ text: String) {
-//        print("Creating a new CustomText")
-//        self.text = text
-//    }
-//}
-
-//struct User: Codable {
-//    let name: String
-//    let address: Address
-//}
-//
-//struct Address: Codable {
-//    let street: String
-//    let city: String
-//}
-
 struct ContentView: View {
-    
-    // GRID LAYOUT
-    //let layout = [
-    //    GridItem(.adaptive(minimum: 80)),
-    //    GridItem(.adaptive(minimum: 80)),
-    //    GridItem(.adaptive(minimum: 80))
-    //]
     
     var body: some View {
         Text("Hello")
-        
-        // SAMPLE JSON DECODER BUTTON WITH SOME SAMPLE JSON DATA
-        //Button("Decode JSON") {
-        //    let input = """
-        //    {
-        //        "name": "Taylor Swift",
-        //        "address": {
-        //            "street": "555, Taylor Swift Avenue",
-        //            "city": "Nashville"
-        //        }
-        //    }
-        //          """
-        //    let data = Data(input.utf8)
-        //    let decoder = JSONDecoder()
-        //    if let user = try? decoder.decode(User.self, from: data) {
-        //        print(user.address.street)
-        //    }
-        //}
-        
-        // SAMPLE NAVIGATIONSTACK WITH NAVIGATIONLINK
-        //NavigationStack {
-        //    List(0..<100) { row in
-        //        NavigationLink("Row \(row)") {
-        //            Text("Detail \(row)")
-        //        }
-        //    }
-        //    .navigationTitle("SwiftUI")
-        //}
-        
-        // NAVIGATIONLINK WITH CUSTOM LABEL
-        //NavigationStack {
-        //    NavigationLink {
-        //        Text("Detail View")
-        //    } label: {
-        //        VStack {
-        //            Text("This is the label")
-        //            Text("So is this")
-        //            Image(systemName: "face.smiling")
-        //        }
-        //        .font(.largeTitle)
-        //    }
-        //    .navigationTitle("SwiftUI")
-        //}
-        
-        // SHOWCASING MODIFIERS FOR AN IMAGE
-        //VStack {
-        //    Image("Example")
-        //        .resizable()
-        //        .scaledToFit()
-        //        .containerRelativeFrame(.horizontal) { size, axis in
-        //            size * 0.8
-        //    }
-        //    Spacer()
-        
-        // SCROLLVIEW WITH A LAZYSTACK.
-        // LAZYSTACKS ARE NICE FOR MEMORY USAGE BECAUSE IT GENERATES DATA WHEN NEEDED.
-        //    ScrollView(.horizontal) {
-        //        LazyHStack(spacing: 10) {
-        //            ForEach(0..<100) {
-        //                CustomText("Item \($0)")
-        //                    .font(.title)
-        //            }
-        //        }
-        //        .frame(maxWidth: .infinity)
-        //    }
-        //}
     }
 }
 
