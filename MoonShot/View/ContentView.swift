efedc7cb258635a6c5e0dde0dc283618da9301cb
Lexter T <lexter.tapawan@gmail.com>
2024-02-07 12:19:42 +0000

    Assets added. Implemented JSON decoder and created generic method for the decoder. Added a date formatter.

---
+++ MoonShot/View/ContentView.swift
@@ -0,0 +1,52 @@
+//
+//  ContentView.swift
+//  MoonShot
+//
+//  Created by Lexter Tapawan on 03/02/2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    
+    let columns = [
+        GridItem(.adaptive(minimum: 150))
+    ]
+    
+    let astronauts: [String: Astronaut] = Bundle.main.decode("astronauts.json")
+    let missions: [Mission] = Bundle.main.decode("missions.json")
+    
+    var body: some View {
+        NavigationStack {
+            ScrollView {
+                LazyVGrid(columns: columns) {
+                    ForEach(missions) { mission in
+                        NavigationLink {
+                            Text("Detail view")
+                        } label: {
+                            VStack {
+                                Image(mission.image)
+                                    .resizable()
+                                    .scaledToFit()
+                                    .frame(width: 100, height: 100)
+                                
+                                VStack {
+                                    Text(mission.displayName)
+                                        .font(.headline)
+                                    Text(mission.formattedLaunchDate)
+                                        .font(.caption)
+                                }
+                                .frame(maxWidth: .infinity)
+                            }
+                        }
+                    }
+                }
+            }
+            .navigationTitle("Moonshot")
+        }
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ MoonShot/View/astronaut.swift
@@ -0,0 +1,14 @@
+//
+//  astronaut.swift
+//  MoonShot
+//
+//  Created by Lexter Tapawan on 07/02/2024.
+//
+
+import Foundation
+
+struct Astronaut: Codable, Identifiable {
+    let id: String
+    let name: String
+    let description: String
+}
